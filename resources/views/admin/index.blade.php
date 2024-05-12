5769fd25697aa8ef4171894456d1dbbb4b6c9b9b
dnabilpc <dnabilpc@gmail.com>
2024-05-12 21:14:12 +0700

    create dashboard view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Digital Laptop</title>
+
+        <link href=" {{asset('bootstrap/css/bootstrap.css')}} " rel="stylesheet">
+        
+    </head>
+    <body>
+        <style>body{background-color: rgba(19, 20, 50, 1);}</style>
+    </body>
+</html>
