452ee7a2789d5d7c5374018ac533fc563ad26b20
Izkyh <eizkyh@gmail.com>
2024-05-13 09:44:02 +0700

    sidebar admin

--- resources/views/kasir/testing.blade.php
+++ resources/views/kasir/testing.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.kasir_main')
 @section('title', 'Testing')
 @section('content')
-    <div class="bg-[#131432] text-white p-2">
+    <div class="bg-[#1C1D42] min-h-screen min-w-full text-white p-2">
         dbwhjkabdkjwakjbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbp
     </div>
 @endsection
