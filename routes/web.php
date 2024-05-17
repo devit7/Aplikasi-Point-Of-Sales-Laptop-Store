fc0e61a7fc85040e43557b6aaa35190f66d9c0d7
rickypratama17 <mrickyp28@gmail.com>
2024-05-17 19:58:35 +0700

    Update sideBar

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('list_customers', function () {
-    return view('admin.list_customers');
+
+Route::get('/', function () {
+    return view('kasir.RiwayatTransaksi');
 });
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('kasir.RiwayatTransaksi');
+    Route::get('list_customers', function () {
+        return view('admin.list_customers');
     });
-});
-
-Route::prefix('admin')->group(function () {
-
-
+    Route::get('/home-admin', function () {
+        return view('layout.kasir_main');
+    });
+    Route::get('/admin', function(){
+        return view('admin.index');
+    });
+    Route::get('/manage_supplier', function(){
+        return view('admin.manage_supplier');
+    });
 });
 
 Route::get('/management_customer', function(){
     return view('kasir.management-customer');
 });
 
-Route::get('/home-admin', function () {
-    return view('layout.kasir_main');
-});
 
 Route::get('/login', function () {
     return view('auth.login');
@@ -46,9 +48,6 @@ Route::get('/RiwayatTransaksi', function () {
     return view('kasir.RiwayatTransaksi');
 });
 
-Route::get('/admin', function(){
-    return view('admin.index');
-});
 
 Route::get('/template', function () {
     return view('template');
