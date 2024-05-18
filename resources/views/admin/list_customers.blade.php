350f32eb24992b407ea2954f56bf12869bc2744e
SiroseJin <139241666+SiroseJin@users.noreply.github.com>
2024-05-18 21:37:20 +0700

    jin

--- resources/views/admin/list_customers.blade.php
+++ resources/views/admin/list_customers.blade.php
@@ -1,75 +1,68 @@
-@extends('layout.admin_main')
-@section('title', 'Customer List')
+@extends('layout.kasir_main')
+@section('title', 'List Customers')
 @section('content')
-    <div class=" p-7 ">
+    <div class="p-8">
         <div class="mb-6">
-            <div class="flex flex-col ">
+            <div class="flex flex-col">
                 <p class="text-[#6b6eb4] text-lg font-semibold">
-                    Customer List
+                    List Customers
                 </p>
-                <p class="text-[#6b6eb4] ">
-                    A List of All of the Customers
-                </p>
-            </div>
-        </div>
-        <div class="bg-[#1C1D42] rounded-md  border border-[#33356F] min-h-[500px]">
-            <div class="flex flex-row justify-between px-4 py-6">
-                <input type="text" class="w-[300px] bg-[#1C1D42] text-white border-2 border-[#33356F] p-2 rounded-md"
-                    placeholder="Search">
-                <div class="flex flex-row gap-2">
-                    <button class="bg-[#aa5800] text-white px-2  rounded-sm">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                        </svg>
-                    </button>
-                    <div
-                        class="flex items-center  rounded-md flex-row gap-2 bg-[#1C1D42] text-[#6b6eb4] border-2 border-[#33356F] px-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
-                        </svg>
-
-                        <select name="" id="" class="bg-[#1C1D42] ">
-
-                            <option value="">Sort by Date</option>
-                            <option value="">Sort by Name</option>
-                            <option value="">Sort by Phone</option>
-                            <option value="">Sort by Email</option>
-                        </select>
-                    </div>
+                <div class="text-[#6b6eb4] flex flex-row">
+                    Manage your customers
                 </div>
             </div>
-            <div class="border-t-2 border-[#33356F] p-4 ">
-                <table class="w-full mt-2 ">
-                    <thead class="bg-[#131432]  text-[#6b6eb4]">
+        </div>
+        <x-tables>
+            <div class="w-full mx-auto mt-2 bg-[#1C1D42] text-[#6b6eb4] p-4 rounded-md">
+                <table class="w-full mt-2" id="table">
+                    <thead class="bg-[#131432]">
                         <tr class="border-b-2 border-[#33356F]">
-                            <th class="py-2 text-[#6b6eb4]">No</th>
-                            <th class="text-[#6b6eb4]">Name</th>
-                            <th class="text-[#6b6eb4]">Email</th>
-                            <th class="text-[#6b6eb4]">Address</th>
-                            <th class="text-[#6b6eb4]">Phone Number</th>
-                            <th class="text-[#6b6eb4]">Action</th>
+                            <th class="py-3">No</th>
+                            <th class="">Name</th>
+                            <th class="">Email</th>
+                            <th class="">Phone</th>
+                            <th class="">Address</th>
+                            <th class="">Action</th>
                         </tr>
                     </thead>
-                    <tbody class="text-[#6b6eb4] text-center">
-                        <tr class="border-b-2 border-[#33356F]">
-                            <td class="py-2">1</td>
-                            <td>Devit</td>
-                            <td>Devitgaming@telkomuniversity.com</td>
-                            <td>Ketintang, Surabaya, Indonesia</td>
-                            <td>+62293293842</td>
-                            <td>
-                                <a href="#" class="bg-[#002D4C] border px-2 border-[#2B4F69] rounded-md">
-                                    View Details
-                                </a>
-                            </td>
-                        </tr>
+                    <tbody class="bg-[#1C1D42]">
+                        @php
+                            $customers = [
+                                ['no' => 1, 'name' => 'John Doe', 'email' => 'john@example.com', 'phone' => '123-456-7890', 'address' => '123 Main St, City, Country'],
+                                ['no' => 2, 'name' => 'Devit', 'email' => 'devitgaming@yahoo.com', 'phone' => '999-999-999', 'address' => 'Surabaya, Sidoarjo'],
+                                ['no' => 3, 'name' => 'Porque', 'email' => 'porque@gmail.com', 'phone' => '122-635-65345', 'address' => 'Loindorlt'],
+                            ];
+                        @endphp
+                        @foreach($customers as $customer)
+                            <tr class="border-b-2 bg-[#1C1D42] border-[#33356F]">
+                                <td class="py-3">{{ $customer['no'] }}</td>
+                                <td>{{ $customer['name'] }}</td>
+                                <td>{{ $customer['email'] }}</td>
+                                <td>{{ $customer['phone'] }}</td>
+                                <td>{{ $customer['address'] }}</td>
+                                <td>
+                                    <button class="bg-[#002D4C] border p-1 border-[#2B4F69] rounded-md">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-600">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    </button>
+                                    <button class="bg-[#002D4C] border p-1 border-[#2B4F69] rounded-md">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+                                    <button class="bg-[#002D4C] border p-1 border-[#2B4F69] rounded-md">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
-        </div>
+        </x-tables>
     </div>
 @endsection
