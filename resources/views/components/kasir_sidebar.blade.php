481d5555dd266b6393f2d2d26f48acf5a77fe473
rickypratama17 <mrickyp28@gmail.com>
2024-05-27 15:32:44 +0700

    Update RIwayat transaksi

--- resources/views/components/kasir_sidebar.blade.php
+++ resources/views/components/kasir_sidebar.blade.php
@@ -36,9 +36,9 @@
                         </div>
                         Customer
                     </a>
-                    <a href="/kasir/transaksi" tabindex="0"
+                    <a href="/kasir/riwayat" tabindex="0"
                         class="flex items-center w-full  px-6 py-3 rounded-lg text-start leading-tight transition-all hover:bg-[#aa5800] hover:bg-opacity-10 hover:text-[#e07946] outline-none
-                    @if (request()->is('kasir/transaksi')) bg-[#aa5800] bg-opacity-10 text-[#e07946] @endif
+                    @if (request()->is('kasir/riwayat')) bg-[#aa5800] bg-opacity-10 text-[#e07946] @endif
                     ">
                         <div class="grid place-items-center mr-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
