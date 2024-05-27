ec16bb29703885856d2a7979767720adec25a470
Rahmatz12 <rahmat12eka@gmail.com>
2024-05-27 10:25:23 +0700

    hahai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
-=======
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
@@ -24,4 +12,3 @@ export default {
     require('tailwind-scrollbar-hide')
   ],
 }
->>>>>>> 7d3a5e520e78cdebc2471e3b604db698aec65219
