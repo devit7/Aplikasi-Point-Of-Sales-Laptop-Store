3bc2d67276b4fd69aba9d18a93ed633784d71bff
dnabilpc <dnabilpc@gmail.com>
2024-06-09 18:56:35 +0700

    Change "reset" button into "back" button

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -185,10 +185,10 @@
                         </div>
 
                         <div class="flex justify-between w-full gap-4 sm:gap-6">
-                            <button type="button" id="button-reset"
-                                class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-700 rounded-md focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                                Reset
-                            </button>
+                            <a href="{{ route('products.admin.index') }}"
+                            class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-gray-600 rounded-md focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-gray-700">
+                                Back
+                            </a>
                             <button type="submit"
                                 class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-green-700 rounded-md focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                                 Create
