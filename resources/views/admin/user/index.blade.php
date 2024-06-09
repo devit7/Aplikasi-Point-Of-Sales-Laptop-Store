a791852ae733e095eece8c658ab37bd67fcc7861
devit <60412293+devit7@users.noreply.github.com>
2024-06-09 23:41:34 +0700

    fix error in nothing table

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -91,9 +91,6 @@
                                     </td>
                                 </tr>
                             @empty
-                                <tr>
-                                    <td colspan="5" class="  text-center">No data available</td>
-                                </tr>
                             @endforelse
                         </tbody>
                     </table>
